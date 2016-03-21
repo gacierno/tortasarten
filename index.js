cf8163586dfc4efea40c4bcddee35553a6b70749
gacierno <gacierno@gmail.com>
2016-03-21 14:50:56 -0300

    f4wf4

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var ver = '0.0.25';
+var ver = '0.0.26';
 
 var express = require("express"); // llama la libreria de metodos
 var path = require('path'); //llama al metodo path para habilitar carpetas
@@ -39,7 +39,7 @@ app.get('/', function(req, res){
 	client.connect();
 
 	res.render('index', {
-		user
+		user, ver
 	});
 });
 app.post('/createtable', function(req, res) {
@@ -48,6 +48,8 @@ app.post('/createtable', function(req, res) {
 	var dbUrl = 'postgres://xpdgjezaphgkak:3QzgObDnFePEcgxuexEGrTYHqT@ec2-54-83-29-133.compute-1.amazonaws.com:5432/d9knjcuq7cub6f';
 	var client = new pg.Client(dbUrl);
 	client.connect();
+
+	console.log("estamos conectados");
 	client.query('CREATE TABLE "users1" (nombre text, apellido text, mail text, password text, secLevel text);');
 });
 
@@ -55,13 +57,13 @@ app.post('/createtable', function(req, res) {
 
 app.get('/workzone', function(req, res){
 	res.render('workzone', 
-		user
+		user, ver
 	);
 });
 
 app.get('/registrationzone', function(req, res){
 		res.render('register', 
-		user
+		user, ver
 	);
 });
 
